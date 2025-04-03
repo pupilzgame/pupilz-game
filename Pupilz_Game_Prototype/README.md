ad27013a9d5524f65382686511710833dc83892b
pupilzgame <pupilznft@gmail.com>
2025-04-02 17:27:40 -0700

    Add files via upload

---
+++ Pupilz_Game_Prototype/README.md
@@ -0,0 +1,2 @@
+# Pupilz Game
+This is the frontend prototype for the Pupilz hybrid TCG.
---
+++ Pupilz_Game_Prototype/src/App.js
@@ -0,0 +1,3 @@
+import React from 'react';
+function App() { return <h1>Pupilz Game Loaded</h1>; }
+export default App;
